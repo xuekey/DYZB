425c155d93f34efb2b9ce7cc50c811fe1dc8821f
Qin <328928303@qq.com>
2018-03-09 17:29:18 +0800

    tabbar

---
+++ DYZHIBO/DYZHIBO/Classes/Main/MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainViewController.swift
+//  DYZHIBO
+//
+//  Created by Apple on 2018/3/9.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self .addChildVCForName(name: "Home")
+        self .addChildVCForName(name: "Living")
+        self .addChildVCForName(name: "Follow")
+        self .addChildVCForName(name: "Profile")
+    }
+
+    func addChildVCForName(name:String){
+        let vc = UIStoryboard(name: name, bundle: nil).instantiateInitialViewController()!
+        self.addChildViewController(vc)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
